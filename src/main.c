cd1e8979cc1ff17b11d3039743cf4bca9ddcba83
cpmachado <cpmachado@protonmail.com>
2023-07-26 12:37:13 +0100

    Minor refactor of main file

--- src/main.c
+++ src/main.c
@@ -7,6 +7,33 @@
 #include "cli.h"
 
 
+/* FUNCTIONS */
+
+int
+computeUnitaryFractions(long int num, long int den, long int *s) {
+	long int n, i;
+
+	for (i = 0; num > 1; i++) {
+		n = den / num + (den % num > 0);
+		s[i] = n;
+		num = num * n - den;
+		den = den * n;
+	}
+
+	if(num > 0) {
+		s[i] = den;
+		i++;
+	}
+	return i;
+}
+
+void
+promptForParameters(long int *num, long int *den) {
+	printf("Let r be a rational, such that 0 < r < 1\n");
+	printf("Input r(n / d): ");
+	scanf(" %ld / %ld", num, den);
+}
+
 int
 main(int argc, char **argv) {
 	long int s[BUFSIZ];
@@ -14,33 +41,22 @@ main(int argc, char **argv) {
 	long int num, den;
 
 	parseFlags(argc, argv);
-
-	printf("Let r be a rational, such that 0 < r < 1\n");
-	printf("Input r(n / d): ");
-	scanf(" %ld / %ld", &num, &den);
-
-	printf("\n");
+	promptForParameters(&num, &den);
 
 	if (num > den) {
 		fprintf(stderr, "r not in ]0, 1[\n");
 		exit(EXIT_FAILURE);
 	}
 
-	while (num > 1) {
-		n = den / num + (den % num > 0);
-		s[i++] = n;
-		num = num * n - den;
-		den = den * n;
-	}
-	if(num > 0) {
-		s[i++] = den;
-	}
-	n = i;
+	n = computeUnitaryFractions(num, den, s);
 
 	for(i = 0; i < n; i++) {
-		printf("%ld ", s[i]);
+		printf("%ld", s[i]);
+		if(i < n - 1) {
+			putchar(',');
+		}
 	}
-	printf("\n");
+	putchar('\n');
 
 	return 0;
 }
